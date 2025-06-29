c0f7b7b113543434fedf2cbf9995083d289cc493
dirshaye <dirshayeyishak2@gmail.com>
2025-06-29 10:41:50 +0100

    Add CI/CD pipeline with GitHub Actions and Docker support

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS base
+WORKDIR /app
+EXPOSE 80
+EXPOSE 443
+
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /src
+COPY ["ScrapeWise.csproj", "./"]
+RUN dotnet restore "ScrapeWise.csproj"
+COPY . .
+WORKDIR "/src"
+RUN dotnet build "ScrapeWise.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "ScrapeWise.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "ScrapeWise.dll"] 
--- obj/Debug/net9.0/ScrapeWise.AssemblyInfo.cs
+++ obj/Debug/net9.0/ScrapeWise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScrapeWise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67f6b8fa4231fc41e4606b6cb5f5bfe86e96d4a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02ef723882b35b26eae640d4e45340d7f4cb7819")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScrapeWise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScrapeWise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
